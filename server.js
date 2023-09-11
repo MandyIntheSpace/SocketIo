f0f368be168c64653201aa0d737cdaa4048e418e
MandyIntheSpace <timsinarewon@gmail.com>
2023-09-11 11:56:29 +0545

    Code Commit

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const dotenv = require('dotenv')
 const { chats } = require('./data/data')
 const cors = require('cors')
 const connectDB = require('./config/db')
+const userRoutes = require('./routes/userRoutes')
 
 const app = express()
 dotenv.config()
@@ -12,6 +13,10 @@ dotenv.config()
 app.use(cors())
 
 connectDB()
+
+
+app.use("/api/user", userRoutes)
+
 
 const port = process.env.PORT || 6000
 
