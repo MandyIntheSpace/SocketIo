3626f9de3d3f01f3f4a844ae2f131a11f2baf994
MandyIntheSpace <timsinarewon@gmail.com>
2023-10-09 10:52:37 +0545

    After the message backend

---
+++ src/components/Miscellaneous/UpdateGroupChatModal.jsx
@@ -0,0 +1,281 @@
+import React, { useState } from "react";
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Button,
+  useDisclosure,
+  IconButton,
+  Box,
+  FormControl,
+  Input,
+  Toast,
+  useToast,
+  Spinner,
+} from "@chakra-ui/react";
+import { ViewIcon } from "@chakra-ui/icons";
+import { ChatState } from "../../context/ChatProvider";
+import UserBadgeItem from "../UserAvator/UserBadgeItem";
+import axios from "axios";
+import UserListItems from "../UserAvator/UserListItems";
+
+export default function UpdateGroupChatModal({ fetchAgain, setFetchAgain }) {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const { selectedChat, setSelectedChat, user } = ChatState();
+  const [groupChatName, setGroupChatName] = useState();
+  const [search, setSearch] = useState("");
+  const [searchResult, setSearchResult] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [renameLoading, setRenameLoading] = useState(false);
+  const toast = useToast();
+
+  const handleRemove = async (userToRemove) => {
+    if (selectedChat.users._id !== user._id && userToRemove._id !== user._id) {
+      toast({
+        title: "Only admins can remove someone!",
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      return;
+    }
+    try {
+      const config = {
+        headers: {
+          Authorization: `Bearer ${user.token}`,
+        },
+      };
+      const { data } = await axios.put(
+        "http://localhost:5000/api/chat/removeMember",
+        {
+          chatId: selectedChat._id,
+          userId: userToRemove._id,
+        },
+        config
+      );
+      userToRemove._id === user._id ? setSelectedChat() : setSelectedChat(data);
+      setFetchAgain(!fetchAgain);
+      setLoading(false);
+    } catch (error) {
+      toast({
+        title: "Error Occured!",
+        description: error.response.data.message,
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      setLoading(false);
+    }
+  };
+
+  const handleRename = async () => {
+    if (!groupChatName) return;
+
+    try {
+      setRenameLoading(true);
+      const config = {
+        headers: {
+          Authorization: `Bearer ${user.token}`,
+        },
+      };
+
+      const { data } = await axios.put(
+        "http://localhost:5000/api/chat/rename",
+        {
+          chatId: selectedChat._id,
+          chatName: groupChatName,
+        },
+        config
+      );
+      setSelectedChat(data);
+      setFetchAgain(!fetchAgain);
+      setRenameLoading(false);
+    } catch (error) {
+      toast({
+        title: "Error Occured!",
+        description: error.response.data.messgae,
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      setRenameLoading(false);
+    }
+    setGroupChatName("");
+  };
+
+  const handleSearch = async (query) => {
+    setSearch(query);
+    if (!search) {
+      return;
+    }
+    try {
+      setLoading(true);
+      const config = {
+        headers: {
+          Authorization: `Bearer ${user.token}`,
+        },
+      };
+      const { data } = await axios.get(
+        `http://localhost:5000/api/user/search?search=${search}`,
+        config
+      );
+      console.log(data);
+      setLoading(false);
+      setSearchResult(data);
+    } catch (error) {
+      toast({
+        title: "Error Occured!",
+        description: "Failed to load search result",
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom-left",
+      });
+      setLoading(false);
+    }
+  };
+
+  const handleAddUser = async (userToAdd) => {
+    if (selectedChat.users.find((u) => u._id === userToAdd._id)) {
+      toast({
+        title: "User Already in group",
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      return;
+    }
+
+    if (selectedChat.groupAdmin._id !== user._id) {
+      toast({
+        title: "Only admins can add someone!",
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      return;
+    }
+
+    try {
+      setLoading(true);
+
+      const config = {
+        headers: {
+          Authorization: `Bearer ${user.token}`,
+        },
+      };
+      const { data } = await axios.put(
+        "http://localhost:5000/api/chat/addMember",
+        {
+          chatId: selectedChat._id,
+          userId: userToAdd._id,
+        },
+        config
+      );
+      setSelectedChat(data);
+      setFetchAgain(!fetchAgain);
+      setLoading(false);
+    } catch (error) {
+      toast({
+        title: "Error Occured!",
+        description: error.response.data.message,
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      setLoading(false);
+    }
+  };
+
+  return (
+    <>
+      <IconButton
+        display={{ base: "flex" }}
+        icon={<ViewIcon />}
+        onClick={onOpen}
+      />
+
+      <Modal isOpen={isOpen} onClose={onClose} isCentered>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader
+            fontSize={"35px"}
+            fontFamily={"Work sans"}
+            display={"flex"}
+            justifyContent={"center"}
+          >
+            {selectedChat.chatName}
+          </ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Box display={"flex"} w={"100%"} flexWrap={"wrap"}>
+              {selectedChat.users.map((u) => (
+                <UserBadgeItem
+                  key={u._id}
+                  user={u}
+                  admin={selectedChat.groupAdmin}
+                  handleFunction={() => handleRemove(u)}
+                />
+              ))}
+            </Box>
+            <FormControl display={"flex"}>
+              <Input
+                placeholder="Chat Name"
+                mb={3}
+                value={groupChatName}
+                onChange={(e) => setGroupChatName(e.target.value)}
+              />
+              <Button
+                variant={"solid"}
+                colorScheme="teal"
+                ml={1}
+                isLoading={renameLoading}
+                onClick={handleRename}
+              >
+                Update
+              </Button>
+            </FormControl>
+            <FormControl>
+              <Input
+                placeholder="Add user to group"
+                mb={1}
+                onChange={(e) => handleSearch(e.target.value)}
+              />
+            </FormControl>
+            {loading ? (
+              <Spinner size={"lg"} />
+            ) : (
+              searchResult?.map((user) => (
+                <UserListItems
+                  key={user._id}
+                  user={user}
+                  handleAccessControl={() => {
+                    handleAddUser(user)
+                    console.log("The button have been triggered")
+                  }}
+                />
+              ))
+            )}
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="red" mr={3} onClick={() => handleRemove()}>
+              Leave Group
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+}
--- src/components/SingleChat/SingleChat.jsx
+++ src/components/SingleChat/SingleChat.jsx
@@ -1,12 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import { ChatState } from "../../context/ChatProvider";
-import { Box, IconButton, Text } from "@chakra-ui/react";
+import {
+  Box,
+  FormControl,
+  IconButton,
+  Spinner,
+  Text,
+  Input,
+} from "@chakra-ui/react";
 import { ArrowBackIcon } from "@chakra-ui/icons";
 import { getSender, getSenderFull } from "../../config/ChatLogic";
 import ProfileModal from "../Miscellaneous/ProfileModal";
+import UpdateGroupChatModal from "../Miscellaneous/UpdateGroupChatModal";
+import axios from "axios";
 
 export default function SingleChat({ fetchAgain, setFetchAgain }) {
+  const [loading, setLoading] = useState(false);
+  const [message, setMessage] = useState([]);
+  const [newMessage, setNewMessage] = useState();
+
   const { user, selectedChat, setSelectedChat } = ChatState();
+  const sendMessages = async (event) => {
+    if (event.key === "Enter" && newMessage) {
+      try {
+        const config = {
+          headers: {
+            "Content-Type": "application/json",
+            Authorizaton: `Bearer ${user.token}`,
+          },
+        };
+        const { data } = await axios.post(
+          "http://localhost:5000/api/message/storeMessage",
+          {
+            content: newMessage,
+            chatId: selectedChat._id,
+          },
+          config
+        );
+        setNewMessage("");
+        setMessage([...message, data]);
+      } catch (error) {}
+    }
+  };
+  const typingHandler = (e) => {
+    setNewMessage(e.target.value);
+  };
 
   return (
     <>
@@ -34,9 +72,51 @@ export default function SingleChat({ fetchAgain, setFetchAgain }) {
                 <ProfileModal user={getSenderFull(user, selectedChat.users)} />
               </>
             ) : (
-              <>{selectedChat.chatName.toUpperCase()}</>
+              <>
+                {selectedChat.chatName.toUpperCase()}
+                {
+                  <UpdateGroupChatModal
+                    fetchAgain={fetchAgain}
+                    setFetchAgain={setFetchAgain}
+                  />
+                }
+              </>
             )}
           </Text>
+          <Box
+            display={"flex"}
+            flexDir={"column"}
+            justifyContent={"flex-end"}
+            p={3}
+            bg={"#E8E8E8"}
+            w={"100%"}
+            h={"100%"}
+            borderRadius={"1g"}
+            overflow={"hidden"}
+            // Messages Here
+          >
+            {loading ? (
+              <Spinner
+                size={"xl"}
+                w={20}
+                h={20}
+                justifyItems={"center"}
+                margin={"auto"}
+              />
+            ) : (
+              <div>{/* message */}</div>
+            )}
+
+            <FormControl onKeyDown={sendMessages} isRequired mt={3}>
+              <Input
+                placeholder="Enter a message"
+                bg={"#E0E0E0"}
+                variant={"filled"}
+                onChange={typingHandler}
+                value={newMessage}
+              ></Input>
+            </FormControl>
+          </Box>
         </>
       ) : (
         <Box
